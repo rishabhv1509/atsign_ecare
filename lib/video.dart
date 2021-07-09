c96a6fcff809f54e222742bd53ff07666b4c5501
Viral Sangani <virals@geekyants.com>
2021-07-09 21:05:53 +0530

    APP_ID added

--- lib/video.dart
+++ lib/video.dart
@@ -3,9 +3,8 @@ import 'dart:async';
 import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
 import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+import 'package:atsign_ecare/utils/setting.dart';
 import 'package:flutter/material.dart';
-
-import '../utils/settings.dart';
 
 class CallPage extends StatefulWidget {
   /// non-modifiable channel name of the page
