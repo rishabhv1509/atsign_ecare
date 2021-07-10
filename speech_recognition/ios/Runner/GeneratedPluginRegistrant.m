3ef4e5d031deebaee432b55f9842a3c20fa6f272
Viral Sangani <virals@geekyants.com>
2021-07-10 21:22:33 +0530

    speeh_recognition package

---
+++ speech_recognition/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<speech_recognition/SpeechRecognitionPlugin.h>)
+#import <speech_recognition/SpeechRecognitionPlugin.h>
+#else
+@import speech_recognition;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SpeechRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"SpeechRecognitionPlugin"]];
+}
+
+@end
